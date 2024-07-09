1cda8839af9bb2c8dc48a4612e92d7bc5a5922d2
Srishylam <srishylamburla1@gmai.com>
2024-07-09 15:19:59 +0530

    vite updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
-  plugins: [react()],
-})
+  // plugins: [react()],
+  build: {
+    plugins: [react()],
+    chunkSizeWarningLimit: 1000, // Adjust this value as needed
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ['lodash'],
+          moment: ['moment'],
+          // Add other manual chunks as needed
+        }
+      }
+    }
+  }
+});
