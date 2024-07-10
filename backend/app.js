9523eda0ab533ad93ef0116af2918d4ba2cf53a6
Srishylam <srishylamburla1@gmai.com>
2024-07-10 13:09:04 +0530

    package change

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,10 @@ app.use(express.json())
 
 __dirname = path.resolve();
 if(process.env.NODE_ENV==='production'){
-  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
   app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "..", 'frontend', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
   })
 }else{
   app.get('/', (req, res) => {
